def9d83ae655a94f082e2846b4d071a163f07e21
Augusto Pohl <you@example.com>
2022-03-21 22:42:20 -0300

    refactor mirage js

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,42 @@
 import { render } from 'react-dom'
-
+import { createServer, Model } from 'miragejs'
 import { App } from './App'
+
+createServer({
+  models: {
+    task: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      tasks: [
+        {
+          id: 1,
+          title: 'Do the dishes',
+          isComplete: false,
+        },
+        {
+          id: 2,
+          title: 'Pay electricity bill',
+          isComplete: false,
+        }
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/tasks', () => {
+      return this.schema.all('task')
+    })
+
+    this.post('/tasks', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('task', data)
+    })
+  }
+})
 
 render(<App />, document.getElementById('root'))

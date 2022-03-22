b544debbae4beefdb58767daa407778af75acf24
Augusto Pohl <you@example.com>
2022-03-22 17:58:12 -0300

    refactor: adjust hook to work properly

--- src/hooks/useTasks.tsx
+++ src/hooks/useTasks.tsx
@@ -1,58 +1,87 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
-
+import { createContext, FormEvent, ReactNode, useContext, useEffect, useState } from 'react';
 interface Task {
     id: number;
     title: string;
     isComplete: boolean;
 }
-
 interface TasksProviderProps {
     children: ReactNode;
 }
-
-
 interface TasksContextData {
-    newTaskTitle: string;
     tasks: Task[];
-    handleCreateNewTask: () => void;
-    setNewTaskTitle: (title: string) => void;
     setTasks: ([]) => void;
+    addNewTask: (title: string) => void;
+    removeTask: (taskId: number) => void;
+    toggleTaskComplete: (taskId: number) => void;
 }
 
 const TasksContext = createContext<TasksContextData>(
     {} as TasksContextData
 );
 
-export function TasksProvider({ children }: TasksProviderProps) {
+export function TasksProvider({ children }: TasksProviderProps): JSX.Element {
     const [tasks, setTasks] = useState<Task[]>([]);
-    const [newTaskTitle, setNewTaskTitle] = useState('');
 
+    useEffect(() => {
+        const tasks = localStorage.getItem("tasksInfo");
+        if (tasks) {
+          setTasks(JSON.parse(tasks));
+        }
+      }, [])
 
-    function handleCreateNewTask() {
-        if (newTaskTitle) {
-            setTasks([
-              ...tasks,
-              {
-                id: new Date().getUTCMilliseconds(),
-                title: newTaskTitle,
-                isComplete: false,
-              }
-            ]);
+      function addNewTask(title: string) {
+        try {
+            const updatedTasks = [...tasks]
       
-            setNewTaskTitle('');
-          } else {
+            if (title) {
+              const newTask = {
+                id: new Date().getUTCMilliseconds(),
+                title: title,
+                isComplete: false,
+              };
+              updatedTasks.push(newTask)
+      
+              setTasks(updatedTasks)
+      
+              localStorage.setItem("tasksInfo", JSON.stringify(updatedTasks));
+      
+            } else {
+              throw Error();
+            }
+          } catch {
             console.log('You cannot add an empty task');
           }
-    }
+      }
+
+      function removeTask(taskId: number) {
+        try {
+            const updatedTasks = [...tasks];
+            const productIndex = updatedTasks.findIndex(task => task.id === taskId)
+      
+            if (productIndex >= 0) {
+              updatedTasks.splice(productIndex, 1);
+      
+              setTasks(updatedTasks);
+      
+              localStorage.setItem('tasksInfo', JSON.stringify(updatedTasks))
+            }
+          } catch {
+            throw Error();
+          }
+      }
+
+      function toggleTaskComplete(taskId: number) {
+        setTasks(tasks.map(task => task.id === taskId ? { ...task, isComplete: !task.isComplete } : { ...task }));
+      }
 
     return (
-        <TasksContext.Provider value={{ tasks: [], setTasks, handleCreateNewTask , newTaskTitle: '', setNewTaskTitle}}>
+        <TasksContext.Provider value={{ tasks, setTasks, addNewTask, removeTask, toggleTaskComplete }}>
             {children}
         </TasksContext.Provider>
     );
 }
 
-export function useTasks() {
+export function useTasks(): TasksContextData {
     const context = useContext(TasksContext);
 
     return context;

f4a68628f524002a49be05a735b4eb2b390100f8
Augusto Pohl <you@example.com>
2022-03-22 22:01:16 -0300

    chore: add page indexes

---
+++ src/pages/EditTasks/index.tsx
@@ -0,0 +1,11 @@
+import { EditTaskList } from "../../components/EditTaskList";
+import { Header } from "../../components/Header";
+
+export default function EditTasks() {
+  return (
+    <>
+      <Header />
+      <EditTaskList />
+    </>
+  )
+}

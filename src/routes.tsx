51f628d28b35af5d5bc5e3f1444d1ce56b07476f
Augusto Pohl <you@example.com>
2022-03-24 13:10:54 -0300

    fix: refactor routes to match edit by id

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,17 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Home from './pages/Home';
-import EditTasks from './pages/EditTasks';
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import EditNote from "./pages/EditNote";
 
 const Routes = (): JSX.Element => {
   return (
     <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/edit" component={EditTasks} />
+      <Route
+        path="/edit/:noteId"
+        children={({ match }: { match: any }) => (
+          <EditNote id={match.params.noteId} />
+        )}
+      />
+      <Route path="/" children={<Home />} />
     </Switch>
   );
 };

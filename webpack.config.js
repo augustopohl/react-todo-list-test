6c1ad9af6b58b3d90c101815c8bd6bdb770c7882
Augusto Pohl <you@example.com>
2022-03-24 22:17:23 -0300

    test webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
